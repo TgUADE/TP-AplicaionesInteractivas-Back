3c0de7d394da23a4e1613c0f29fdbe7b7fdfd8f7
JoacoMarc <jmarcoff@uade.edu.ar>
2025-08-22 18:39:44 -0300

    fix: Ensure category_id is not nullable in Product entity
    - Updated the `Product` entity to set the `category_id` column as non-nullable, enforcing data integrity in the database.

--- marketplace/src/main/java/com/entity/Product.java
+++ marketplace/src/main/java/com/entity/Product.java
@@ -45,6 +45,6 @@ public class Product {
     private long stock;
 
     @ManyToOne
-    @JoinColumn(name = "category_id")
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 }
