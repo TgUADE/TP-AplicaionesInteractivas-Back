87d70c3456e583f6df735c10cd7c3c86d389ae51
Chiara <cgiannetto@uade.edu.ar>
2025-09-02 10:01:41 -0300

    Entity y DTO de ordenes

---
+++ marketplace/src/main/java/com/entity/dto/OrderRequest.java
@@ -0,0 +1,16 @@
+package com.entity.dto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class OrderRequest {
+    private UUID id;
+    private UUID userId;
+    private List<UUID> productIds;
+
+    // Fecha de orden:
+    // private LocalDateTime createdAt;
+}
