7e3ca83c078ae35b994c8e3549995abc8d925d15
Chiara <cgiannetto@uade.edu.ar>
2025-09-02 10:44:40 -0300

    Service (falta completar cuando user este listo)

---
+++ marketplace/src/main/java/com/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import com.entity.Order;
+import com.entity.dto.OrderRequest;
+import com.exceptions.OrderDuplicateException;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderService {
+    List<Order> getOrders();
+    Optional<Order> getOrderById(UUID orderId);
+    Order createOrder(OrderRequest request) throws OrderDuplicateException;
+    Order updateOrder(UUID orderId, OrderRequest request);
+    Order deleteOrder(UUID orderId);
+}
---
+++ marketplace/src/main/java/com/service/OrderServiceImpl.java
@@ -0,0 +1,54 @@
+package com.service;
+
+import com.entity.Order;
+import com.entity.dto.OrderRequest;
+import com.repository.CategoryRepository;
+import com.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+//    @Autowired
+//    private UserService userService;
+
+    @Autowired
+    private ProductService productService;
+
+
+
+
+    public List<Order> getOrders(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderById(UUID orderId){
+        return orderRepository.findById(orderId);
+    }
+
+    public Order createOrder(OrderRequest orderRequest){
+        
+
+    }
+
+    public Order updateOrder(OrderRequest orderRequest){
+
+    }
+
+    public Order deleteOrder(UUID orderId){
+
+    }
+
+
+}
+
+
+
