269b8d823436ce2eea602110f693becfdc14b8b0
Chiara <cgiannetto@uade.edu.ar>
2025-09-02 10:53:25 -0300

    Order controller

--- marketplace/src/main/java/com/controllers/OrderController.java
+++ marketplace/src/main/java/com/controllers/OrderController.java
@@ -3,11 +3,60 @@ package com.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.entity.Order;
+import com.entity.dto.OrderRequest;
+import com.exceptions.OrderNotFoundException;
+import com.service.OrderService;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 @RequestMapping("orders")
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
 
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders() {
+        return ResponseEntity.ok(orderService.getOrders());
+    }
 
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(@PathVariable UUID orderId) throws OrderNotFoundException {
+        Order result = orderService.getOrderById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException());
+        return ResponseEntity.ok(result);
+    }
 
+    @PostMapping
+    public ResponseEntity<Object> createOrder(@RequestBody OrderRequest orderRequest) {
+        Order result = orderService.createOrder(orderRequest);
+        return ResponseEntity.created(URI.create("/orders/" + result.getId())).body(result);
+    }
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<Order> updateOrder(@PathVariable UUID orderId, @RequestBody OrderRequest orderRequest)
+            throws OrderNotFoundException {
+        Order result = orderService.updateOrder(orderId, orderRequest);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Order> deleteOrder(@PathVariable UUID orderId)
+            throws OrderNotFoundException {
+        orderService.deleteOrder(orderId);
+        return ResponseEntity.noContent().build();
+    }
 }
