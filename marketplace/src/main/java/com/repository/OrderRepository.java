dff6f4d128ee7768dfb7b7e97eb1583190ee4b30
Chiara <cgiannetto@uade.edu.ar>
2025-09-02 10:49:39 -0300

    Repo orders

--- marketplace/src/main/java/com/repository/OrderRepository.java
+++ marketplace/src/main/java/com/repository/OrderRepository.java
@@ -2,10 +2,20 @@ package com.repository;
 
 import com.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    // Buscar todas las órdenes de un usuario
+    @Query("SELECT o FROM Order o WHERE o.user.id = ?1")
+    List<Order> findByUserId(UUID userId);
+
+    // Buscar órdenes que contengan cierto producto
+    @Query("SELECT o FROM Order o JOIN o.products p WHERE p.id = ?1")
+    List<Order> findByProductsId(UUID productId);
+
 }
